83ff52a29e3b55f45eab56f3474cca22e11137e4
Francesco Cappelli <francesco.cappelli.developer@gmail.com>
2020-12-27 01:31:46 +0000

    remove empty space

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ app.get('/health-check', (req: any, res) => res.send('ok'));
 app.get('/todo', getTodoList);
 app.get('/todo/:id', getTodoById);
 app.post('/todo', postTodo);
-app.put('/todo/:id', putTodoById );
+app.put('/todo/:id', putTodoById);
 
 app.listen(3000, () => {
 	console.log('App is listening on port 3000!');
